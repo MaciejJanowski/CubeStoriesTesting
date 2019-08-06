91101d9d6d9f3e027a8c0563d6b7cb090d1fbde6
MaciejJanowski <maciej.janowski@insight-centre.org>
2019-08-06 14:54:09 +0100

    example script refinement

--- example.py
+++ example.py
@@ -6,9 +6,9 @@ metdadataparameters={
 }
 
 cubeparameters={
-    "cube":"ChildrenBySizeFamily",  #Key value of cube
-    "dimensions":["FamilySize"], #Dimensions from cube
-    "measures":["Children"], #Measures
+    "cube":"PopulationPrinicipalStatus",  #Key value of cube
+    "dimensions":["EconomicStatus","Gender"], #Dimensions from cube
+    "measures":["Population"], #Measures
     "hierdimensions": #Hierachical Dimension with granularity level
         {"RefArea":{ #Dimension key
             "selected_level":"CTY" # selected level
@@ -21,27 +21,23 @@ cubeparameters={
 AnalysisPipeline={  ##Dictionary pair: [Pattern Key]:[List of Pattern Parameters]
     
     "LeagueTab":{   
-       "columns_to_order":["FamilySize"],
+       "columns_to_order":["Population"],
        "order_type":"desc",
-       "number_of_records":20
+       "number_of_records":10
    },
    "DissFact":{
         "dim_to_dissect":"RefArea"
-    },
-   "MeasCount":{
-       "count_type":"min"
-   }
-
+    }
 
 }
-
+#######DO NOT CHANGE########################
 a=storyTeller(metadataparameters=metdadataparameters,
                 cubeparameters=cubeparameters,
                 patternparameters=AnalysisPipeline)
 
 
 a.tellStory()
-
+#############################################
 x=(a.showStory())
 
-print(x["LeagueTab"])
+print(x)
